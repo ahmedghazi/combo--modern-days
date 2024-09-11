92ce60872449b0699d3a973fdf76c8c9349956d1
ahmedghazi <atmet.ghazi@gmail.com>
2024-09-11 10:37:52 +0200

    fix

--- web/app/config/website.ts
+++ web/app/config/website.ts
@@ -1,8 +1,9 @@
 const website = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: "Site Name", // Navigation and Site Title
+  title: "JOUUUE ! Editions", // Navigation and Site Title
   titleAlt: "SN", // Title for JSONLD
-  description: "...",
+  description:
+    "JOUUUE ! Editions : Groupe d'édition littéraire urbain et ambitieux",
   headline: "", // Headline for schema.org JSONLD
   url: "https://aaa.fr", // Domain of your site. No trailing slash!
   image: "", // Used for SEO
